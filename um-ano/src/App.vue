5e763e78869062120a7bb583be2bc4ab5a3c44de
RSobrinho <rafarrsobrinho@hotmail.com>
2022-08-17 11:24:07 -0400

    HTML e CSS da Página principal finalizado (bem porcamente pois fiz isso em 2 horas, pra conseguir finalizar até dia 22)

--- um-ano/src/App.vue
+++ um-ano/src/App.vue
@@ -1,5 +1,291 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <header>
+      <img src="./assets/logoHeart.png" alt="logoHeart" />
+      <nav>
+        <button class="dropdown">
+          Nossos Meses :D
+          <img src="./assets/iconDown.png" alt="iconDown" />
+        </button>
+        <button class="login">Log In</button>
+      </nav>
+    </header>
+    <main>
+      <section class="intro">
+        <h1 class="title__intro">
+          Amor da minha vida, essa simples página é só uma homenagem para
+          demonstrar tudo o que já vivemos em somente 1 ano, mas ainda por cima
+          mostrar tudo o que
+          <span>
+            ainda podemos conquistar juntos, porque com a ajuda um do outro tudo
+            que desejamos vai se tornar possível, te amo.</span
+          >
+        </h1>
+      </section>
+      <section class="subintro">
+        <img src="./assets/img1.png" alt="img1" />
+        <p class="subtitle__subintro">
+          Coloquei essa foto, sinceramente porque ficou boa neste estilo da
+          página, eu literamente estou escrevendo isso dia 16 de agosto, na
+          pressa pra te fazer uma surpresa, então espero que goste meu amor. E
+          essa foto foi uma de muitas que ainda vou colocar de wallpaper do
+          celular :D
+        </p>
+      </section>
+      <section class="transition">
+        <h2>Então Vamos Lá</h2>
+        <img src="./assets/smile.png" alt="smile" />
+      </section>
+      <section class="earlierAugust">
+        <div class="titleBlueBar">
+          <h3>Antes de agosto... quando tudo começou</h3>
+        </div>
+        <div class="content_earlierAugust">
+          <div class="left__earlierAugust">
+            <img src="./assets/img2.1.png" alt="imgLeft" />
+          </div>
+          <div class="right__earlierAugust">
+            <img src="./assets/img2.2.png" alt="imgRight" />
+            <p>
+              Bom, aqui é aquela euforia, aquela paixão, que aos poucos vai se
+              tornando amor de verdade. Quando eu te stalkeei vi suas fotos no
+              insta, eu imediatamente apaixonei e senti que essa poderia ser a
+              oportunidade de encontrar a pessoa certa para mim, que me entenda,
+              que saiba conversar das mesmas coisas que eu, que saiba
+              simplemente me amar de uma forma que eu me sentisse completo todos
+              os dias. E simplesmente foi a maior oportunidade que eu não deixei
+              passar na minha vida.
+            </p>
+          </div>
+        </div>
+      </section>
+      <section class="august">
+        <div>
+          <h3>Agosto, quando aquele pedido foi feito...</h3>
+        </div>
+        <div class="upContent__august">
+          <img src="./assets/img3.1.png" alt="imgUp" />
+        </div>
+        <div class="downContent__august">
+          <p>
+            Nessa foto a direita, foi o parque que eu te pedi em namoro, foi o
+            dia que daí em diante eu entendi o que era ter alguem por perto,
+            alguem que te considera, que te quer junto de você, que te ama.
+          </p>
+          <img src="./assets/img3.2.png" alt="imgDown" />
+          <p>
+            Por incrível que pareça, eu me sentia confiante no dia, pois
+            simplesmente parecia (e éra) a escolha mais certa a ser tomada. E
+            aquele vídeo brega não me impediu de dizer aquelas palavras.
+          </p>
+        </div>
+      </section>
+      <section class="september">
+        <div class="titlePinkBar">
+          <h3>Setembro, o primeiro de muitos meses</h3>
+        </div>
+        <div class="upContent__september">
+          <img src="./assets/img4.png" alt="imgUp" />
+          <video controls>
+            <source src="./assets/vid1.mp4" type="video/mp4" />
+            Your browser dows not support the video tag
+          </video>
+        </div>
+        <div class="downContent__september">
+          <p>
+            Aqui começou a nossa vida de namorados, onde tudo parecia estar
+            dando errado, mas um tentava acalmar o outro pra ninguém surtar de
+            adequando as novas atividades, como responder o zapzap a cada 10
+            minutos ao invés de 2 horas, e problemas fora do relacionamento, mas
+            que afetavam a gente indiretamente. Nessa foto, foi a festa lá na
+            minha tia, aonde você conheceu a minha familia inteira (pelo que eu
+            me lembre), e todos te adoraram de cara, e decidiram te “agregar” na
+            familia kkkkk.
+          </p>
+        </div>
+      </section>
+      <section class="october">
+        <div>
+          <h3>Outubro, só 2 meseszinhos, mal imaginávamos</h3>
+        </div>
+        <div class="upContent__october">
+          <img src="./assets/img5.1.png" alt="upImages" />
+          <img src="./assets/img5.2.png" alt="upImages" />
+        </div>
+        <div class="centerContent__october">
+          <p>
+            Daí em diante, a coisa foi só crescendo, nós fomos nos
+            desenvolvendo, saindo, nos ajudando nos nossos problemas
+            (psicológicos principalmente) mas nunca deixando de querer a outra
+            pessoa por perto a todo momento. Nosso “primeiro encontro de casais
+            sem querer”, o que foi engraçado, e sua formatura, aonde aquela
+            maquiagem não combinou mas você continuava linda e maravilhosa do
+            mesmo jeito.
+          </p>
+        </div>
+        <div class="downContent__october">
+          <img src="./assets/img5.3.png" alt="upImages" />
+          <img src="./assets/img5.4.png" alt="upImages" />
+        </div>
+      </section>
+      <section class="november">
+        <div class="titleBlueBar">
+          <h3>Novembro, e com isso nosso amor aumentava + e +</h3>
+        </div>
+        <img src="./assets/img6.png" alt="imgs" />
+      </section>
+      <section class="december">
+        <div>
+          <h3>Dezembro, essas bubbles tavam boas né? kkkk</h3>
+        </div>
+        <div class="downContent__december">
+          <img src="./assets/img7.png" alt="imgs" />
+        </div>
+      </section>
+      <section class="january">
+        <div class="titlePinkBar">
+          <h3>Janeiro, e a nossa viagem juntos!</h3>
+        </div>
+        <div class="carousel__january">
+          <div class="titleDay__carouselCard">
+            <h3>Dia 21</h3>
+          </div>
+          <div class="contentBox__carouselCard">
+            <button class="leftButton">
+              <img src="./assets/iconLeftBlue.png" alt="leftButton" />
+            </button>
+            <div class="content">
+              <img src="./assets/img1.png" alt="img" />
+            </div>
+            <button class="rightButton">
+              <img src="./assets/iconRightBlue.png" alt="rightButton" />
+            </button>
+          </div>
+        </div>
+        <div class="carousel__january">
+          <div class="titleDay__carouselCard">
+            <h3>Dia 21</h3>
+          </div>
+          <div class="contentBox__carouselCard">
+            <button class="leftButton">
+              <img src="./assets/iconLeftBlue.png" alt="leftButton" />
+            </button>
+            <div class="content">
+              <img src="./assets/img1.png" alt="img" />
+            </div>
+            <button class="rightButton">
+              <img src="./assets/iconRightBlue.png" alt="rightButton" />
+            </button>
+          </div>
+        </div>
+        <div class="carousel__january">
+          <div class="titleDay__carouselCard">
+            <h3>Dia 21</h3>
+          </div>
+          <div class="contentBox__carouselCard">
+            <button class="leftButton">
+              <img src="./assets/iconLeftBlue.png" alt="leftButton" />
+            </button>
+            <div class="content">
+              <img src="./assets/img1.png" alt="img" />
+            </div>
+            <button class="rightButton">
+              <img src="./assets/iconRightBlue.png" alt="rightButton" />
+            </button>
+          </div>
+        </div>
+      </section>
+      <section class="fevMarAprMay">
+        <div class="titleBlueBar">
+          <h3>Fevereiro, março, abril, maio: Pouca foto é foda kkkk</h3>
+        </div>
+        <div>
+          <img src="./assets/img8.png" alt="imgs" />
+        </div>
+        <div>
+          <p>
+            Simplesmente, nosso amor já tinha se consolidado, nós estávamos nos
+            conhecendo cada vez mais, e a vontade de permanecer com você, de
+            conversar com você, de lembrar de você, aumentava cada vez e cada
+            vez mais. Eu não entendia, e pra falar a verdade ainda não entendo,
+            mas tenho certeza que sou mais feliz do que era antes de te
+            conhecer. Cada momento, cada ida no cinema, me fazia ainda mais
+            feliz, mesmo nos momentos tristes, que machucam, que ferem e que
+            marcam negativamente, você e eu sempre permanecemos juntos.
+          </p>
+        </div>
+      </section>
+      <section class="june">
+        <div>
+          <h3>Junho, nossa viagem pra pira :0</h3>
+        </div>
+        <div class="content__june">
+          <div class="leftContent__june">
+            <img src="./assets/img9.1.png" alt="img" />
+            <p>
+              Pra falar bem a verdade, eu preferi a última viagem, mas eu adorei
+              essa também já que você estáva junto :D. Foi nessa viagem que eu
+              entendi que no futuro, nós que vamos estar indo para estes
+              lugares, junto de amigos, familia, ou somente nós 2, mas com nossa
+              independência conquistada, e muitos outros sonhos para serem
+              alcançados. Juntos, sempre vamos empurrar o outro para frente,
+              para o crescimento mútuo, com consistência e clareza dos
+              objetivos, sendo eles individuais mas que podem ser impulsionados
+              com alguém ao seu lado, como também nossos objetivos juntos como
+              um casal.
+            </p>
+          </div>
+          <div class="rightContent_june">
+            <img src="./assets/img9.2.png" alt="img" />
+          </div>
+        </div>
+      </section>
+      <section class="july">
+        <div class="titleBlueBar">
+          <h3>Julho, separados porém só na distância calma amor</h3>
+        </div>
+        <div>
+          <p>
+            Nessa viagem de júlho, eu queria muito você lá, mas não dá para ter
+            tudo que deseja não é mesmo kkkkkkkk. Apesar disso, permanecemos em
+            contato e foi como se nunca tivessemos saído de perto um do outro.
+            Eu me tornando bruxo e “Wine Drinker” (botei em inglês pra ficar
+            chique) e você se tornando mais linda a cada dia (como que é
+            possível isso meu Deus).
+          </p>
+        </div>
+        <div>
+          <img src="./assets/img10.png" alt="imgs" />
+        </div>
+      </section>
+      <section class="heartMaster">
+        <h3>
+          Passou um ano inteiro, e nesse meio nós crescemos, melhoramos, nos
+          adaptamos, um com a ajuda do outro, mas parece que pssou 5 minutos
+          igual essa página da web kkkkkk, porém ainda temos muitas experiências
+          para contruir juntos, e nesse caminho nos tornar felizes, um com o
+          outro.
+        </h3>
+      </section>
+      <section class="ourFuture">
+        <div>
+          <h2 class="title__ourFuture">Em breve, juntos</h2>
+        </div>
+        <div>
+          <img src="./assets/img12.png" alt="imgs" />
+        </div>
+        <div>
+          <button class="btn__ourFuture">
+            <h2>NÃO CLIQUE!</h2>
+          </button>
+        </div>
+      </section>
+    </main>
+    <footer>
+      <h2>Todos os direitos reservados a nós</h2>
+      <img src="./assets/logoHeart.png" alt="logoHeart" />
+    </footer>
+  </div>
 </template>
 
 <script>
@@ -7,12 +293,383 @@ export default {}
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap');
+
+* {
+  box-sizing: border-box;
+  padding: 0;
+  margin: 0;
+}
+
+html {
+  font-size: 62.5%;
+  font-family: 'Inter', sans-serif;
+}
+
+body {
+  overflow-x: hidden;
+  width: 100vw;
+  height: 100vh;
+  background-image: url('./assets/heart.png'),
+    radial-gradient(
+      circle,
+      rgba(255, 216, 216, 1) 0%,
+      rgba(255, 237, 239, 1) 50%,
+      rgba(255, 233, 255, 1) 100%
+    );
+  background-repeat: repeat-y, repeat;
+  background-position: center 0rem, 0px, 0px;
+  display: flex;
+  justify-content: center;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  z-index: 10;
+  width: 120rem;
+  display: flex;
+  flex-direction: column;
+  row-gap: 15rem;
+}
+
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+nav {
+  display: flex;
+  gap: 5rem;
+}
+
+h3 {
+  font-size: 4.5rem;
+  color: #ebf2f8;
+  padding: 3rem 0rem;
+}
+
+p {
+  font-size: 2.8rem;
+  color: #10202b;
+}
+
+img {
+  border-radius: 1.5rem;
+}
+
+video {
+  width: 30rem;
+  height: 60rem;
+}
+
+.dropdown {
+  width: 30rem;
+  height: 6rem;
+  border-style: none;
+  border-radius: 1.5rem;
+  font-size: 2.8rem;
+  line-height: 2.4rem;
+  background: #10202b;
+  color: #ebf2f8;
+}
+
+.dropdown img {
+  margin-left: 1rem;
+}
+
+.login {
+  width: 20rem;
+  height: 6rem;
+  border-style: none;
+  border-radius: 1.5rem;
+  font-size: 2.8rem;
+  line-height: 2.4rem;
+  background: #fc4444;
+  color: #10202b;
+}
+
+main {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  row-gap: 20rem;
+}
+
+section {
+  display: flex;
+  align-items: center;
+}
+
+.title__intro {
+  width: 100rem;
+  font-size: 4rem;
+  line-height: 4rem;
+  color: #10202b;
+  font-weight: 600;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+}
+.title__intro span {
+  color: #d90f0f;
+}
+
+.subtitle__subintro {
+  margin-left: 3rem;
+  font-size: 2.4rem;
+  line-height: 2.4rem;
+  color: #10202b;
+  text-align: left;
+  width: 26rem;
+}
+
+.transition {
+  padding: 1rem 0rem;
+  width: 100rem;
+  background: #fc4444;
+  border-radius: 1.5rem;
+  font-size: 6rem;
+  line-height: 4rem;
+  color: #ebf2f8;
+  justify-content: space-around;
+}
+
+.earlierAugust {
+  flex-direction: column;
+  row-gap: 10rem;
+}
+
+.titleBlueBar {
+  background-color: #10202b;
+  border-radius: 1rem;
+  width: 120rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.content_earlierAugust {
+  display: flex;
+  justify-content: space-between;
+  column-gap: 10rem;
+}
+
+.right__content {
+  max-width: 60rem;
+  text-align: center;
+}
+
+.right__content p {
+  margin-top: 2rem;
+}
+
+.august {
+  background-color: #ff9f9f;
+  flex-direction: column;
+  border-radius: 2rem;
+  row-gap: 10rem;
+  padding: 0rem 4rem 6rem 4rem;
+  text-align: center;
+}
+
+.august h3 {
+  color: #10202b;
+}
+
+.downContent__august {
+  display: flex;
+  column-gap: 2rem;
+  align-items: center;
+}
+
+.september {
+  flex-direction: column;
+  row-gap: 10rem;
+}
+
+.titlePinkBar {
+  background-color: #d90f0f;
+  border-radius: 1rem;
+  width: 120rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.upContent__september {
+  width: 120rem;
+  display: flex;
+  column-gap: 15rem;
+}
+
+.september p {
+  width: 100rem;
+  text-align: center;
+}
+
+.october {
+  flex-direction: column;
+  row-gap: 10rem;
+
+  background-color: #4a687c;
+  border-radius: 2rem;
+
+  padding: 0rem 4rem 8rem 4rem;
+
+  text-align: center;
+}
+
+.october div {
+  display: flex;
+  align-items: center;
+  column-gap: 10rem;
+}
+
+.october p {
+  color: #ebf2f8;
+}
+
+.november {
+  flex-direction: column;
+  row-gap: 10rem;
+}
+
+.december {
+  flex-direction: column;
+  row-gap: 10rem;
+  background-color: #4a687c;
+  border-radius: 2rem;
+}
+
+.downContent__december {
+  padding: 0rem 8rem 12rem 8rem;
+}
+
+.january {
+  flex-direction: column;
+  row-gap: 10rem;
+}
+
+.carousel__january {
+  width: 120rem;
+  display: flex;
+  flex-direction: column;
+  row-gap: 10rem;
+  align-items: center;
+}
+
+.carousel__january h3 {
+  color: #10202b;
+}
+
+.contentBox__carouselCard {
+  display: flex;
+  align-items: center;
+  column-gap: 15rem;
+}
+
+.contentBox__carouselCard button {
+  background: transparent;
+  border-style: none;
+}
+
+.fevMarAprMay {
+  flex-direction: column;
+  row-gap: 10rem;
+}
+
+.fevMarAprMay p {
+  width: 100rem;
+  text-align: center;
+}
+
+.june {
+  flex-direction: column;
+  row-gap: 10rem;
+
+  background: #ff9f9f;
+  border-radius: 2rem;
+  padding: 0rem 8rem 12rem 8rem;
+}
+
+.june h3 {
+  color: #10202b;
+}
+
+.content__june {
+  display: flex;
+  align-items: center;
+  column-gap: 5rem;
+}
+
+.june p {
+  margin-top: 20rem;
+  text-align: center;
+}
+
+.july {
+  flex-direction: column;
+  row-gap: 10rem;
+}
+.july p {
+  text-align: center;
+  width: 100rem;
+}
+
+.heartMaster {
+  background-image: url('./assets/img11.png');
+  background-repeat: no-repeat;
+  background-position: center;
+
+  width: 160rem;
+  height: 140rem;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+.heartMaster h3 {
+  font-size: 3.5rem;
+  line-height: 4rem;
+  color: #ebf2f8;
+  text-align: center;
+  width: 60rem;
+  margin-bottom: 5rem;
+}
+
+.ourFuture {
+  flex-direction: column;
+  row-gap: 20rem;
+}
+
+.title__ourFuture {
+  color: #d90f0f;
+  font-size: 15rem;
+  text-align: center;
+}
+
+.btn__ourFuture {
+  width: 52rem;
+  height: 15rem;
+  border-style: none;
+  border-radius: 3rem;
+  background: #d90f0f;
+}
+
+.btn__ourFuture h2 {
+  font-size: 5rem;
+  color: #ebf2f8;
+}
+
+footer {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+
+  background: #fc4444;
+}
+
+footer h2 {
+  color: #ebf2f8;
+  font-size: 3rem;
 }
 </style>
